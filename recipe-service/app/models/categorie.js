a84ab600969560870d79623fa20b48bec834dc8c
enaho murphy <enahpmurphy@saitama.local>
2017-07-16 21:55:27 +0100

    Add category schmema

---
+++ recipe-service/app/models/categorie.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Category title is required'],
+    lowercase: true,
+    validate: {
+      validator(v) {
+        return new Promise(resolve => {
+          resolve(v && v.length > 2);
+        });
+      },
+      message: 'Category title should be greater than two characters'
+    },
+    unique: true
+  },
+  description: {
+    type: String
+  }
+}, {
+  timestamps:
+    { createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+});
+
+/**
+ * @Class
+ */
+class Recipe {
+  /**
+   * @param {string} id
+   * @return {object} returns a user
+   */
+  getCategoryById(id) {
+    return this.findById(id);
+  }
+
+  /**
+   * @param {int} limit
+   * @param {int} page
+   * @return {object} returns an object
+   */
+  getAllCategories(limit = 10, page) {
+    const query = { limit, sort: { createdAt: -1 } };
+    if (page) {
+      query.page = limit * page;
+    }
+    return {
+      result: this.find(query).fetch(),
+      count: this.count()
+    };
+  }
+}
+CategorySchema.loadClass(Recipe);
+
+module.exports = mongoose.model('Category', CategorySchema);
--- recipe-service/server.js
+++ recipe-service/server.js
@@ -3,22 +3,22 @@ const express = require('express');
 const config = require('./app/config');
 
 const app = express();
-const port = 3000 || process.env.PORT
+const port = 3000 || process.env.PORT;
 
 
 app.get('/', (req, res) => {
-  res.send("Hello world")
+  res.send('Hello world');
 });
 
 require('./app/config/db')(config)
   .then(() => {
-    app.listen(port, (err) => {
+    app.listen(port, err => {
       if (err) {
-        throw new Error(err.message)
+        throw new Error(err.message);
       }
-      console.log(`recipe service is running on port ${port}`)
+      console.log(`recipe service is running on port ${port}`);
     });
   })
-  .catch(err =>  { throw new Error(`Connection error ${err.message}`) });
+  .catch(err => { throw new Error(`Connection error ${err.message}`); });
 
 module.exports = app;
